9943842b01ed3eb88a07765302785a0f4c68e51a
Guillermo Ruvalcaba <102990400+Rubal04@users.noreply.github.com>
2024-10-24 23:50:30 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+# Load data
+# df = pd.read_csv('data/final_workout_df_cleaned.csv')
+
+
+st.title("🎈 Data Exploration")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
