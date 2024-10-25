1f4a309141e60aeb8ffd0c95aa390d3d37e6fca5
Guillermo Ruvalcaba <102990400+Rubal04@users.noreply.github.com>
2024-10-25 01:15:10 +0000

    Update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 
 # Load data
-# df = pd.read_csv('data/final_workout_df_cleaned.csv')
+df = pd.read_csv('data/final_workout_df_cleaned.csv')
 
 
 st.title("🎈 Data Exploration")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+# CCreate filter
+columna_a_filtrar = st.selectbox('Selecciona una columna', df.columns)
+valor_filtro = st.slider('Selecciona un valor', int(df[columna_a_filtrar].min()), int(df[columna_a_filtrar].max()))
+
