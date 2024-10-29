c331361cb9087577d6bce55dca852aaf1b59d9cf
Guillermo Ruvalcaba <102990400+Rubal04@users.noreply.github.com>
2024-10-29 22:32:54 +0000

    Las updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,17 @@ df = pd.read_csv('data/final_workout_df_cleaned.csv')
 
 st.title("🎈 Data Exploration")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, Head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 # CCreate filter
 columna_a_filtrar = st.selectbox('Selecciona una columna', df.columns)
 valor_filtro = st.slider('Selecciona un valor', int(df[columna_a_filtrar].min()), int(df[columna_a_filtrar].max()))
 
+# Filtrar los datos
+print (columna_a_filtrar)
+print (valor_filtro)
+
+df_filtrado = df[df[columna_a_filtrar] == valor_filtro]
+
+# Mostrar un gráfico
+st.line_chart(df_filtrado)
